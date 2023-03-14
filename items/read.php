b61e1bd001472b0966a52005bdbcdbd9f3d8f5ee
Agustin Karkocki <agustin.karkocki@gmail.com>
2023-03-14 16:37:28 -0300

    add read

--- items/read.php
+++ items/read.php
@@ -1,40 +1,31 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
 
-include_once '../config/Database.php';
-include_once '../class/Items.php';
+$email = (isset($_GET['email']) && $_GET['email']) ? $_GET['email'] : null;
 
-$database = new Database();
-$db = $database->getConnection();
- 
-$items = new Items($db);
+if (($handle = fopen("reporte.csv", "r")) !== FALSE) {
+    $read=file("reporte.csv");
+    $reg = 0;
+    
+    $arrayValues = array();
+    foreach ($read as $v) {
+        $linea = str_replace(chr(13).chr(10), ',', $v);
+        list($email_, $saludo, $nombre)=explode(",",$linea);
+        
 
-$items->id = (isset($_GET['id']) && $_GET['id']) ? $_GET['id'] : '0';
-
-$result = $items->read();
-
-if($result->num_rows > 0){    
-    $itemRecords=array();
-    $itemRecords["items"]=array(); 
-	while ($item = $result->fetch_assoc()) { 	
-        extract($item); 
-        $itemDetails=array(
-            "id" => $id,
-            "name" => $name,
-            "description" => $description,
-			"price" => $price,
-            "category_id" => $category_id,            
-			"created" => $created,
-            "modified" => $modified			
-        ); 
-       array_push($itemRecords["items"], $itemDetails);
-    }    
-    http_response_code(200);     
-    echo json_encode($itemRecords);
-}else{     
-    http_response_code(404);     
+        if ($email_ == $email) {
+            array_push($arrayValues,[$email,$saludo, $nombre]);
+        }
+    }
+    
+    $final_array = ["items" => $arrayValues, "count"=> count($arrayValues)];
+    http_response_code(200);
+    echo json_encode($final_array);
+} else {
+    http_response_code(404);
     echo json_encode(
         array("message" => "No item found.")
     );
-} 
+}
